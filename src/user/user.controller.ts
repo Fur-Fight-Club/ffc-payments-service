5480ec9c030ddc7810e00355c015daefa7a0e306
ZERRAI Sami <sami.zerrai@gmail.com>
2023-05-10 18:10:10 +0200

    feat: add apiparam

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiTags, ApiParam } from "@nestjs/swagger";
 
 @Controller("user")
 @ApiTags("User controller")
@@ -8,6 +8,11 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(":id")
+  @ApiParam({
+    description: "The id of the user",
+    name: "id",
+    type: Number,
+  })
   async getUser(@Param("id", ParseIntPipe) id: number) {
     return this.userService.getUser(id);
   }
